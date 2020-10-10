519a5506b57ad12451ca2263b0c8ede38626b527
Urinboev Abdushukur <abdushukururinboev@gmail.com>
2020-10-10 11:41:07 +0500

    This program is done using javascript

---
+++ js/index.js
@@ -0,0 +1,37 @@
+function Palindrom(){
+    let birinchi = document.getElementById("first-name").Value;
+    let result;
+
+    
+    if (birinchi > 0; birinchi--) {
+        result = "the word is palindrome";
+    } else {
+        result = "the word is not palindrome"
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return document.getElementById("result").value = result;
+}
+
+//     for( var i = palindrom.length; i > 0; i-- )
+//     {
+//         if( palindrom[i] = palindrom.charAt(palindrom.length)-1 )
+//         {
+//             document.write('the word is palindrome.');
+//         }else{
+//             document.write('the word is not palindrome!');
+//         }
+//     }
+// }
+// checkPalindrom('wordthatwillbechecked');
